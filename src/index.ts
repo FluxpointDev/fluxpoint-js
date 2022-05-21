7c781fa00a7bf22d6b5a19be0ad9d627dfd695b1
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2022-05-20 23:33:58 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export * from "./FluxpointClient";
+export * from "../src/Structures/Classes/Extra/Misc";
+export * from "../src/Structures/Classes/SFW/index";
+export * from "../src/Structures/Classes/NSFW/index";
