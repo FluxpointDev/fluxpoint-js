ca7b8a74a1f374156c8beb5f9d61b9c07a117746
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2022-05-20 22:39:13 -0400

    NSFW

---
+++ src/Structures/Classes/NSFW/index.ts
@@ -0,0 +1,14 @@
+import { FluxpointClient } from "../../../FluxpointClient";
+import { Gifs } from "./Classes/Gifs";
+import { Images } from "./Classes/Images";
+
+export class NSFW {
+    private client: FluxpointClient;
+    public images: Images;
+    public gifs: Gifs;
+    constructor(client: FluxpointClient) {
+        this.client = client;
+        this.images = new Images(client);
+        this.gifs = new Gifs(client);
+    };
+};
