970b2598229fde8fd7389d8986527305097761f2
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2022-05-29 23:28:21 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from "./FluxpointClient";
 export { Misc } from "./Structures/Classes/Extra/Misc";
-export { SFW } from "../src/Structures/Classes/SFW/index";
+export { SFW } from "./Structures/Classes/SFW/index";
 export { Gifs as GifsSFW } from "./Structures/Classes/SFW/Classes/Gifs";
 export { Images as ImagesSFW } from "./Structures/Classes/SFW/Classes/Images";
 export { NSFW } from "./Structures/Classes/NSFW/index";
